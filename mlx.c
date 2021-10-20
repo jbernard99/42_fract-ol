2d338debde4b7878a599c5fb3784fe70478b2adb
Julien Bernard <jbernard@c2r1p3.42quebec.com>
2021-10-20 16:02:56 -0400

    First push

---
+++ mlx.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 10:30:50 by jbernard          #+#    #+#             */
+/*   Updated: 2021/10/20 13:10:10 by jbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+int	main()
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1280, 720, "MiniLibX testing...");
+	mlx_loop(mlx_ptr);
+	return (0);
+}
